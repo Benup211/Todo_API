ec088bd5f7d23c3d88f5f3ecf2b73cea9f2233a6
benup211 <benup211@gmail.com>
2024-07-29 16:34:09 +0545

    model created

--- todo/views.js
+++ todo/views.js
@@ -1,8 +1,17 @@
 const express=require('express');
 const router=express.Router();
+const Todo=require('./models');
 
-router.get('/',(req,res)=>{
-    res.status(200).json({"message":"todo view"})
+router.get('/',async(req,res)=>{
+    const todos=await Todo.findAll();
+    if (todos.length==0){
+        const todo=await Todo.create({title:"t1",description:"lorem",status:"ongoing",userID:1});
+        res.status(200).json({"todos":todo})
+    }
+    res.status(200).json({"todos":todos})
 });
-
+router.get('/:id',async(req,res)=>{
+    const todo=await Todo.findByPk(req.params.id);
+    res.status(200).json({"todo":todo})
+});
 module.exports=router
